630cbaffd25bf720b29607357ca0585b4e8ce5bd
ninaflaaten <nifl1349@gmail.com>
2025-10-30 23:40:27 +0100

    rekkefølge

--- src/app/components/WishGallery.tsx
+++ src/app/components/WishGallery.tsx
@@ -4,24 +4,21 @@ export function WishGallery() {
   return (
     <>
       <WishItem
-        title="kvikklunsj-sjokolade"
-        imageSrc="https://images-tastehub.mdlzapps.cloud/images/7c05889a-c617-46b0-9825-4ba6a6e8d994.png"
-        link="https://www.google.com/search?q=kvikk+lunsj+sjokoladeplate"
+        title="engangskamera"
+        imageSrc="/camera.png"
+        link="https://www.google.com/search?q=engangskamera"
         imageClassName="animate-slow-spin"
       />
-
       <WishItem
         title="bunadskjorte"
         imageSrc="https://sidserk.no/cdn/shop/files/bunadskjorte_kari_tyrihans.webp?v=1741095493&width=320"
         link="https://www.google.com/search?q=bunadskjorte"
       />
-
       <WishItem
         title="cute pysjsett"
         imageSrc="https://png.pngtree.com/png-clipart/20210905/original/pngtree-pajama-style-home-clothing-png-image_6704795.jpg"
         link="https://www.google.com/search?q=cute+pajama+set"
       />
-
       <WishItem
         title="tøfler (str. 37/38)"
         imageSrc="https://png.pngtree.com/png-vector/20250329/ourmid/pngtree-cozy-white-fluffy-slippers-for-winter-and-home-comfort-wear-png-image_15899252.png"
@@ -32,15 +29,11 @@ export function WishGallery() {
         imageSrc="https://www.kerastase.no/-/media/project/loreal/brand-sites/kerastase/emea/nx/products/genesis/packshots/serum-anti-chute-fortifiant/serum-anti-chute-fortifiant-genesis-90ml-01-kerastase.png?rev=48624a94db18409a8b66ca46c14b4f8a"
         link="https://www.google.com/search?q=kerastase+genesis"
       />
-       <WishItem
+
+      <WishItem
         title="bunadsko"
         imageSrc="/bunad-sko.png"
         link="https://www.google.com/search?q=bunadsko"
-      />
-      <WishItem
-        title="kérastase Nutritive"
-        imageSrc="/kerastase-night-serum.png"
-        link="https://www.google.com/search?q=kerastase+nutritive+night+serum&sca_esv=490573eed39ba66d&ei=p4oDabGJN6DYwPAP3cXx6AQ&oq=kerastase+nu+serum&gs_lp=Egxnd3Mtd2l6LXNlcnAiEmtlcmFzdGFzZSBudSBzZXJ1bSoCCAEyBhAAGAcYHjIGEAAYBxgeMgYQABgHGB4yBhAAGAcYHjIGEAAYBxgeMgYQABgHGB4yBhAAGAcYHjIGEAAYBxgeMgYQABgHGB4yBhAAGAcYHkiiEVC5BFinBnACeAGQAQCYAV6gAZ8BqgEBMrgBAcgBAPgBAZgCBKACtgHCAgoQABiwAxjWBBhHwgINEAAYgAQYsAMYQxiKBZgDAIgGAZAGCpIHAzMuMaAH6g-yBwMxLjG4B6wBwgcFMC4xLjPIBw8&sclient=gws-wiz-serp"
       />
       <WishItem
         title="bublizz godis"
@@ -48,33 +41,41 @@ export function WishGallery() {
         link="https://www.google.com/search?q=bublizz"
       />
       <WishItem
+        title="kérastase Nutritive"
+        imageSrc="/kerastase-night-serum.png"
+        link="https://www.google.com/search?q=kerastase+nutritive+night+serum&sca_esv=490573eed39ba66d&ei=p4oDabGJN6DYwPAP3cXx6AQ&oq=kerastase+nu+serum&gs_lp=Egxnd3Mtd2l6LXNlcnAiEmtlcmFzdGFzZSBudSBzZXJ1bSoCCAEyBhAAGAcYHjIGEAAYBxgeMgYQABgHGB4yBhAAGAcYHjIGEAAYBxgeMgYQABgHGB4yBhAAGAcYHjIGEAAYBxgeMgYQABgHGB4yBhAAGAcYHkiiEVC5BFinBnACeAGQAQCYAV6gAZ8BqgEBMrgBAcgBAPgBAZgCBKACtgHCAgoQABiwAxjWBBhHwgINEAAYgAQYsAMYQxiKBZgDAIgGAZAGCpIHAzMuMaAH6g-yBwMxLjG4B6wBwgcFMC4xLjPIBw8&sclient=gws-wiz-serp"
+      />
+
+      <WishItem
         title="morgenkåpe (burgunder/rosa/beige)"
         imageSrc="https://png.pngtree.com/png-vector/20240207/ourmid/pngtree-watercolor-bathrobe-clip-art-png-image_11679299.png"
         link="https://www.google.com/search?q=morgenkåpe+dame"
       />
-
       <WishItem
         title="Rema1000 / Coop / Tise / Zara / Ikea / Kid"
         imageSrc="/giftcard.png"
         link="https://www.google.com/search?q=gavekort+matbutikk"
       />
-
       <WishItem
         title="ansiktsskrubb"
         imageSrc="https://cdn-icons-png.flaticon.com/512/1271/1271525.png"
         link="https://www.google.com/search?q=ansiktsskrubb+sensitiv"
       />
-
       <WishItem
-        title="simple armbånd"
-        imageSrc="https://static.vecteezy.com/system/resources/thumbnails/022/110/443/small/butterfly-gold-bracelet-with-pink-gemstone-png.png"
-        link="https://tise.com/t/MyeL6PVob"
+        title="kvikklunsj-sjokolade"
+        imageSrc="https://images-tastehub.mdlzapps.cloud/images/7c05889a-c617-46b0-9825-4ba6a6e8d994.png"
+        link="https://www.google.com/search?q=kvikk+lunsj+sjokoladeplate"
+        imageClassName="animate-slow-spin"
       />
-
       <WishItem
         title="hoodie (burgunder/navy), strl small"
         imageSrc="/hoodie.png"
         link="https://www.ginatricot.com/no/klaer/gensere/hettegensere/iconic-hoodie-217354203"
+      />
+      <WishItem
+        title="simple armbånd"
+        imageSrc="https://static.vecteezy.com/system/resources/thumbnails/022/110/443/small/butterfly-gold-bracelet-with-pink-gemstone-png.png"
+        link="https://tise.com/t/MyeL6PVob"
       />
     </>
   );
